2fc2de09f78f390856142d1788cf321071fbd045
Eiko Thomas <13103714+OkieOth@users.noreply.github.com>
2025-10-22 22:17:17 +0200

    project init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+[![ci](https://github.com/OkieOth/ic0bra/actions/workflows/test.yml/badge.svg?branch=main&event=push)](https://github.com/OkieOth/ic0bra/actions/workflows/test.yml)
+[![go report card](https://goreportcard.com/badge/github.com/OkieOth/ic0bra)](https://goreportcard.com/report/github.com/OkieOth/ic0bra)
+
+
 # ic0bra
-Extension to the cobra library to allow an interactive walk through a cli tool start configuration
+Extension to the cobra library to allow an interactive walk through a cli tool start configuration.
+
+As additional feature the library allows to include a history for configured flags
---
+++ _examples/history/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/okieoth/ic0bra"
+	"github.com/okieoth/ic0bra_examples/helper"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "simple",
+	Short: "Example for ic0bra integration with flag history",
+	Long:  `Example for ic0bra integration with flag history to provide an advanced interactive option for command line tools`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if cmdToCall, err := ic0bra.RunInteractiveWithHistory(cmd); err == nil {
+			if cmdToCall != nil {
+				cmdToCall.Run(cmdToCall, args)
+			}
+		} else {
+			fmt.Println("error while running in interactive mode:", err)
+		}
+	},
+}
+
+func init() {
+	helper.AddSubCommands(rootCmd)
+}
+
+func main() {
+	fmt.Println("I am an advanced example for the use of the basic interactive feature around the cobra lib.")
+	rootCmd.Execute()
+}
